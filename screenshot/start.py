45f5e29c1fc194455f9e0f0a3708b4b7de287d2e
齊尼曼 <97031067+chienniman@users.noreply.github.com>
2023-06-12 23:46:50 +0800

    ch1-screenshot

---
+++ screenshot/start.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options=Options()
+options.chrome_executable_path="chromedriver.exe"
+
+driver=webdriver.Chrome(options=options)
+driver.maximize_window()
+driver.get("https://www.boris.idv.tw/")
+driver.save_screenshot("screenshot.png")
+driver.close()
